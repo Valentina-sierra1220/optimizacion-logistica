96c062e910219d44f51fdfde44ceedf8cc43e941
mariajosehincapie18 <mjhincapie18@gmail.com>
2026-09-23 22:31:38 -0500

    recorrido de asignaciones y prueba con caso pequeño funcionando ademas de las 16 combinaciones seleccionadas

--- src/fuerza_bruta.py
+++ src/fuerza_bruta.py
@@ -1,4 +1,27 @@
-from  auxiliares import es_asignacion_valida
+def fuerza_bruta(paquetes,camiones):
+    
+    def generar_asignaciones(indice, asignacion):
 
-def fuerza_bruta(paqutes,camiones):
-    pass
+        if indice == len(paquetes):
+            print("asigancion completa", asignacion)
+            return
+
+        paquete = paquetes[indice]
+        print("estoy procesando: ", paquete["id"])
+        for camion in camiones:
+            print(paquete["id"],"->", camion["id"])
+            nueva_asignacion = asignacion.copy()
+            nueva_asignacion[paquete["id"]] = camion["id"]
+            generar_asignaciones(indice + 1,nueva_asignacion )
+
+    generar_asignaciones(0,{})
+
+
+
+   
+
+
+if __name__ == "__main__":
+    print("Prueba de fuerza bruta")
+    camiones, paquetes = cargar_caso_prueba("datos/caso_pequeno.txt")
+    fuerza_bruta(paquetes, camiones)
